23a10d8c9223028c5818044cf8c87d43c8995a1b
Oleg Pavlish <110381858+olegeng@users.noreply.github.com>
2023-09-15 17:40:34 +0300

    Delete pythonProject5 directory

--- pythonProject5/dofd-05-olegeng-main/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Header() {
-  return (
-    <div className="container is-fluid title is-1 has-text-centered">
-      Market admin panel. You can add items, see all items, edit and delete them
-    </div>
-  );
-}
--- pythonProject5/dofd-05-olegeng-main/src/components/NotFound.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function NotFound() {
-  return <p className="title is-1 has-text-centered">Page not found</p>;
-}
